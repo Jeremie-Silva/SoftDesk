e83d120d976983c875cbdfd899040a181c72bd19
my-tricks <silva.jeremie93@gmail.com>
2023-12-29 21:55:12 +0100

    DEV

--- SoftDesk/soft_desk_api/admin.py
+++ SoftDesk/soft_desk_api/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
+from .models import Contributor, Project, Issue, Comment
 
-# Register your models here.
+
+@register(Contributor)
+class ContributorAdmin(ModelAdmin):
+    list_display = ("username", "age", "can_be_contacted", "can_data_be_shared")
+    fields = ("user", "age", "can_be_contacted")
+
+
+@register(Project)
+class ProjectAdmin(ModelAdmin):
+    list_display = ("name", "author", "type", "display_contributors")
+    fields = ("name", "author", "type", "contributors")
+
+    def display_contributors(self, obj):
+        return ", ".join(
+            [contributor.username for contributor in obj.contributors.all()]
+        )
+    display_contributors.short_description = "contributors"
+
+
+@register(Issue)
+class IssueAdmin(ModelAdmin):
+    list_display = ("project", "assigned_contributor", "state", "priority", "label")
+    fields = ("project", "assigned_contributor", "state", "priority", "label")
+
+
+@register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ("issue",)
+    fields = ("issue",)
--- SoftDesk/soft_desk_api/models.py
+++ SoftDesk/soft_desk_api/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contributor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="contributor")
+    age = models.PositiveSmallIntegerField(default=18)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+    is_staff = False
+
+    @property
+    def username(self):
+        return self.user.username
+
+    def __str__(self):
+        return self.username
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, blank=True)
+    author = models.ForeignKey(
+        to=Contributor, on_delete=models.PROTECT, related_name="authored_projects"
+    )
+    contributors = models.ManyToManyField(to=Contributor, related_name="projects")
+    type = models.CharField(max_length=100, blank=True, choices=[
+        ("BE", "Back-End"), ("FE", "Front-End"), ("IOS", "iOS"), ("AND", "Android"),
+    ])
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    project = models.ForeignKey(
+        to=Project, on_delete=models.PROTECT, related_name="issues"
+    )
+    assigned_contributor = models.ForeignKey(
+        to=Contributor, on_delete=models.SET_NULL, related_name="assigned_issues",
+        null=True, blank=True
+    )
+    state = models.CharField(max_length=100, default="TO DO", choices=[
+        ("TO DO", "TO DO"), ("In Progress", "In Progress"), ("Finished", "Finished"),
+    ])
+    priority = models.CharField(max_length=100, blank=True, choices=[
+        ("LOW", "LOW"), ("MEDIUM", "MEDIUM"), ("HIGH", "HIGH"),
+    ])
+    label = models.CharField(max_length=100, blank=True, choices=[
+        ("BUG", "BUG"), ("FEATURE", "FEATURE"), ("TASK", "TASK"),
+    ])
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.project}] {self.label} : {self.priority} "
+
+
+class Comment(models.Model):
+    issue = models.ForeignKey(
+        to=Issue, on_delete=models.CASCADE, related_name="comments",
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
