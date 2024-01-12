ff08062cbaeba669a22e1d08f3f23182306e5299
my-tricks <silva.jeremie93@gmail.com>
2024-01-12 14:56:21 +0100

    DEV

--- SoftDesk/soft_desk_api/permissions.py
+++ SoftDesk/soft_desk_api/permissions.py
@@ -1,16 +1,31 @@
-from rest_framework.permissions import BasePermission
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class CustomPermission(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        try:
-            user_auth_tuple = JWTAuthentication().authenticate(request)
-            if user_auth_tuple:
-                request.user, _ = user_auth_tuple
-                return True
-                # TODO : add logical permissions
-        except AuthenticationFailed as exc:
-            pass
-        return False
+class IsContributor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """Permission for /endpoint/{id}"""
+        if request.method in SAFE_METHODS:
+            return request.user.contributor in obj.contributors.all()
+        else:
+            return request.user.contributor == obj.author
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class IsOwner(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         """Permission for /endpoint/{id}"""
+#         if request.method not in SAFE_METHODS:
+#             return request.user.contributor in obj.contributors.all()
+#         return False
--- SoftDesk/soft_desk_api/views.py
+++ SoftDesk/soft_desk_api/views.py
@@ -1,7 +1,8 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
-from .permissions import CustomPermission
+from .permissions import IsContributor
 from .models import Contributor, Project, Issue, Comment
 from .serializers import ContributorSerializer, ProjectSerializer, \
     IssueSerializer, CommentSerializer
@@ -10,25 +11,34 @@ from .serializers import ContributorSerializer, ProjectSerializer, \
 class ContributorViewSet(ModelViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [IsAuthenticated, IsContributor]
 
 
 class ProjectViewSet(ModelViewSet):
-    queryset = Project.objects.all()
+    queryset = Project.objects.all()  # redefined in get_queryset()
     serializer_class = ProjectSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [IsAuthenticated, IsContributor]
+
+    def get_queryset(self):
+        if self.action == "list":
+            return self.request.user.contributor.projects_contribution.all()
+        else:
+            return Project.objects.filter(id=self.kwargs.get("pk"))
 
 
 class IssueViewSet(ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [IsAuthenticated, IsContributor]
+
+    def get_queryset(self):
+        return Issue.objects.filter(project__contributors=self.request.user.contributor)
 
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [IsAuthenticated, IsContributor]
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
