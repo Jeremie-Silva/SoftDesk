f26110994fb2f4d067daa747aca221f05f1be57d
my-tricks <silva.jeremie93@gmail.com>
2024-02-16 17:45:13 +0100

    DEV

--- SoftDesk/soft_desk_api/views.py
+++ SoftDesk/soft_desk_api/views.py
@@ -1,8 +1,10 @@
 from django.db.models import QuerySet
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from .permissions import IsContributorOrOwner, IsOwnerOrReadOnly
 from .models import Contributor, Project, Issue, Comment
 from .serializers import ContributorSerializer, ProjectSerializer, \
@@ -19,6 +21,10 @@ class ContributorViewSet(ModelViewSet):
             return Contributor.objects.all()
         else:
             return Contributor.objects.filter(id=self.kwargs.get("pk"))
+
+    def destroy(self, request, *args, **kwargs):
+        self.perform_destroy(self.request.user)
+        return Response(status=HTTP_200_OK)
 
 
 class ProjectViewSet(ModelViewSet):
